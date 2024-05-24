308154700139c35fc4e5d246b3ba5e1126e013d0
Juan David <juangiraldopb@gmail.com>
2024-05-23 22:17:34 -0500

    Sizes related window

---
+++ Game.cpp
@@ -0,0 +1,165 @@
+#include "Game.h"
+
+//Private functions
+void Game::initVariables() {
+    this->window = nullptr;
+
+    //Game Logic
+    this->securityDialog = "Bienvenido a la Universidad Catolica del Oriente!!";
+}
+
+void Game::initWindow() {
+
+    this->window = new sf::RenderWindow(sf::VideoMode::getDesktopMode(), "Recorrido Virtual UCO", sf::Style::Fullscreen);
+
+    this->window->setFramerateLimit(120);
+}
+
+void Game::initTextures() {
+
+}
+
+
+void Game::initFonts() {
+    if(!this->font.loadFromFile("C:/Users/Juan David/Desktop/visita_virtual/Fonts/Pearce.ttf")){
+        std::cout << "ERROR::GAME::INITFONTS::failed to load font!" << "\n";
+    }
+}
+
+void Game::initText() {
+
+    sf::Vector2<unsigned int> windowSize = this->window->getSize();
+
+    this->uiText.setPosition(3 * ((float)windowSize.x/3), 490.f);
+    this->uiText.setFont(this->font);
+    this->uiText.setCharacterSize(30);
+    this->uiText.setFillColor(sf::Color(86, 86, 86));
+    this->uiText.setString("none");
+}
+
+void Game::initBox() {
+
+    sf::Vector2<unsigned int> windowSize = this->window->getSize();
+
+    this->box.setPosition(30.f, 3 * ((float) windowSize.y/4));
+    this->box.setSize(sf::Vector2f(((float) windowSize.x)-60, 250.f));
+    this->box.setFillColor(sf::Color(204, 204, 204, 180));
+    this->box.setOutlineColor(sf::Color(102, 102, 102));
+    this->box.setOutlineThickness(6.f);
+}
+
+void Game::initWorld() {
+
+    this->worldBackgroundTex.loadFromFile("C:/Users/Juan David/Desktop/visita_virtual/Textures/background4.jpg");
+    this->worldBackground.setTexture(this->worldBackgroundTex);
+
+    sf::Vector2<unsigned int> windowSize = this->window->getSize();
+    sf::Vector2<unsigned int> textureSize = this->worldBackgroundTex.getSize();
+
+    float scaleX = (float) windowSize.x / textureSize.x;
+    float scaleY = (float) windowSize.y / textureSize.y;
+
+    this->worldBackground.scale(scaleX, scaleY);
+}
+
+void Game::initSecurityGuard() {
+    this->characterTexture.loadFromFile("C:/Users/Juan David/Desktop/visita_virtual/Textures/officer.png");
+    this->securityGuard.setTexture(this->characterTexture);
+
+    this->securityGuard.setPosition(850, 30);
+}
+
+
+//Constructors & Destructors
+Game::Game() {
+    this->initVariables();
+    this->initWindow();
+    this->initTextures();
+    this->initWorld();
+    this->initSecurityGuard();
+    this->initFonts();
+    this->initText();
+    this->initBox();
+}
+Game::~Game() {
+    delete this->window;
+}
+
+//Accessors
+const bool Game::running() const {
+    return this->window->isOpen();
+}
+
+//Functions
+void Game::pollEvents() {
+    //Event polling
+    while(this->window->pollEvent(this->ev)){
+        switch (this->ev.type) {
+            case sf::Event::Closed:
+                this->window->close();
+                break;
+            case sf::Event::KeyPressed:
+                if (this->ev.key.code == sf::Keyboard::Escape){
+                    this->window->close();
+                }
+                break;
+        }
+    }
+}
+
+void Game::updateMousePositions() {
+    /*
+        Updated mouse postions:
+        - Mouse position relative to window.
+     */
+
+    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+}
+
+void Game::update() {
+    this->pollEvents();
+
+    this->updateMousePositions();
+
+    this->updateText();
+}
+
+void Game::updateText() {
+
+    this->uiText.setString(this->securityDialog);
+}
+
+void Game::renderText(sf::RenderTarget& target) {
+    target.draw(this->uiText);
+}
+
+void Game::renderWorld() {
+    this->window->draw(this->worldBackground);
+}
+
+void Game::renderSecurityGuard() {
+    this->window->draw(this->securityGuard);
+}
+
+void Game::render() {
+
+    /*
+        - clear old frame
+        - render objects
+        - display frame window
+
+        Renders the game objects.
+     */
+
+    this->window->clear();
+
+    //Draw world
+    this->renderWorld();
+
+    //Draw game objects
+    this->renderSecurityGuard();
+    this->window->draw(this->box);
+    this->renderText(*this->window);
+
+    this->window->display();
+}
---
+++ Game.h
@@ -0,0 +1,77 @@
+#ifndef VISITAVIRTUAL_GAME_H
+#define VISITAVIRTUAL_GAME_H
+
+#include "iostream"
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+
+/*
+    Class that acts as game engine.
+    Wrapper class.
+ */
+
+class Game {
+private:
+    //Variables
+    //Window
+    sf::RenderWindow* window;
+    //sf::VideoMode videoMode;
+    sf::Event ev;
+
+    //Mouse positions
+    sf::Vector2i mousePosWindow;
+
+    //Resources
+    sf::Font font;
+
+    //World
+    sf::Texture worldBackgroundTex;
+    sf::Sprite worldBackground;
+
+    //Character
+    sf::Texture characterTexture;
+    sf::Sprite securityGuard;
+
+    //Text
+    sf::Text uiText;
+
+    //Game Logic
+    std::string securityDialog;
+
+    //Game objects
+    sf::RectangleShape box;
+
+    //Private functions
+    void initVariables();
+    void initWindow();
+    void initTextures();
+    void initWorld();
+    void initSecurityGuard();
+    void initFonts();
+    void initText();
+    void initBox();
+public:
+    //Constructors & Destructors
+    Game();
+    virtual ~Game();
+
+    //Accessors
+    const bool running() const;
+
+    //Functions
+    void pollEvents();
+    void updateMousePositions();
+    void updateText();
+    void update();
+    void render();
+
+    void renderText(sf::RenderTarget& target);
+    void renderSecurityGuard();
+    void renderWorld();
+};
+
+
+#endif //VISITAVIRTUAL_GAME_H
